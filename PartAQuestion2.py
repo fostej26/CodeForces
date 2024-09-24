4721a0b71c9ce7ef0df7e7430bf5ea40d8257189
fostej26 <fostej26@mcmaster.ca>
2024-09-24 19:20:05 -0400

    question 2

---
+++ PartAQuestion2.py
@@ -0,0 +1,24 @@
+def power_consumption(n, p1, p2, p3, t1, t2, interval_list):
+    total = 0
+    previous_end = interval_list[0][0]
+ 
+    for i in range(n):
+        start, end = interval_list[i]
+        total += (end - start) * p1
+ 
+        if i > 0:
+            idle_time = start - previous_end
+            if idle_time <= t1:
+                total += idle_time * p1
+            elif idle_time <= t1 + t2:
+                total += t1 * p1 + (idle_time - t1) * p2
+            else:
+                total += t1 * p1 + t2 * p2 + (idle_time - t1 - t2) * p3
+        
+        previous_end = end
+ 
+    return total
+ 
+n, p1, p2, p3, t1, t2 = map(int, input().split())
+interval_list = [list(map(int, input().split())) for _ in range(n)]
+print(power_consumption(n, p1, p2, p3, t1, t2, interval_list))
