48706cde60b447b32fb7ca6bdc7f7a6d9490befd
Jacob Foster <fostej26@mcmaster.ca>
2024-09-24 20:59:40 -0700

    part b question 5 (easy)

--- PartBQuestion5.py
+++ PartBQuestion5.py
@@ -1,3 +1,14 @@
-def messageTransmissionError(string):
-    return
+def messageError(string):
+    
+    n = len(string)
+    for i in range((n // 2) + 1, n):
+        check1 = string[:i]
+        check2 = string[-i:]
+        if check1 == check2:
+            print("YES")
+            print(check1)
+            return
+    print("NO")
 
+string = input().strip()
+messageError(string)
