89395f913cdf19c737253495ea50cd1f779aedc0
Jacob Foster <fostej26@mcmaster.ca>
2024-09-24 20:22:27 -0700

    part b questions

---
+++ PartBQuestion2.py
@@ -0,0 +1,16 @@
+def correctSolution(alice, bob):
+    alice.sort()
+    index = 1
+    if alice[0] == "0":
+        while alice[0] == "0" and index < len(alice):
+            alice[0], alice[index] = alice[index], alice[0]
+            index += 1
+    if alice == bob:
+        print("OK")
+    else:
+        print("WRONG_ANSWER")
+
+alice = [char for char in input()]
+bob = [char for char in input()]
+
+correctSolution(alice, bob)
---
+++ PartBQuestion4.py
@@ -0,0 +1,17 @@
+def mountainBiking(n, elevationList):
+    incline = 0
+    max_incline = 0
+    for index in range(1, n):
+        if elevationList[index] > elevationList[index - 1]:
+            incline += elevationList[index] - elevationList[index - 1]
+        else:
+            if incline > max_incline:
+                max_incline = incline
+            incline = 0
+    if incline > max_incline:
+        max_incline = incline
+    return max_incline
+
+n = int(input())
+elevationList = list(map(int, input().split()))
+print(mountainBiking(n, elevationList))
